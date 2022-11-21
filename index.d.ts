2fd980ae2036a5e7ec5c5bbcd34a5786b83bca43
Ryan Martin <eqpoqpe@gmail.com>
2022-11-21 22:43:29 +0800

    upload

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,10 @@
 export declare type SiphonOptions = {
   success(res: any): void;
+
+  // server status error
   fail(err: any): void;
+
+  // network error or URL invalided
   invalid(err: any): void;
 };
 export declare function Siphon(options: SiphonOptions): void;
