9a4db17364d4d65a1fb9d2b19490bf271d373b3d
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-11-25 19:38:58 +0800

    Update App.js

--- example/App.js
+++ example/App.js
@@ -1,9 +1,1 @@
-import { Siphon } from "siphon";
 
-export default function App() {
-  const [profile, setProfile] = useSiphonQuery("GET_PROFILE");
-  
-  return (
-    <div>{profile.displayName}</div>
-  );
-};
