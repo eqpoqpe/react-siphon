9bd8d4c4c5abb446f92dbbf06588969dec238e01
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-12-01 18:18:46 +0800

    Update App.js

--- example/App.js
+++ example/App.js
@@ -1,16 +1,22 @@
-import { SiphonClient, SiphonProvider } from "react-siphon";
+import { SiphonClient, SiphonProvider, useSiphon } from "react-siphon";
 
 const siphon = new SiphonClient();
 
 siphon.createSecret([
   {
-    key: "GET_PROFILE"
+    key: "GET_PROFILE",
+    url: ""
   }
 ]);
+
+function Trap() {
+  const { url } = useSiphon({ key: "GET_RPOFILE" });
+}
 
 export default function App() {
   return (
     <SiphonProvider client={siphon}>
+      <Trap />
     </SiphonProvider>
   );
 };
